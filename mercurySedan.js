b4b23cb6242f69a21746c6f5bd412ba223550ede
BGZhao <92346786+BGZhao@users.noreply.github.com>
2022-01-11 21:18:07 -0600

    Changes review
    changes review

--- mercurySedan.js
+++ mercurySedan.js
@@ -3,11 +3,11 @@ import Vehicle from "./vehicle.js";
 class Car extends Vehicle {
     constructor(make, model, year, color, mileage) {
         super(make, model, year, color, mileage);
-        this.maxPassengers = 5;
+        this.maxPassengers = 6;
         this.passenger = 0;
         this.numberOfWheels = 4;
-        this.maxSpeed = 160;
-        this.fuel = 10;
+        this.maxSpeed = 200;
+        this.fuel = 100;
         this.scheduleService = false;
     }
 
@@ -16,7 +16,7 @@ class Car extends Vehicle {
             console.log("engine has started.");
         } else {
             return this.started = false;
-            console.log("no fuel";
+            console.log("no fuel");
         }
     }
 
